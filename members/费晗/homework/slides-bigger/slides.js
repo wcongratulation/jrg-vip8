6d5a6e89d4b48dd8ff71e7a16bc8c699a52596d0
beyond <964959033@qq.com>
2016-06-24 10:51:04 +0800

    s

--- members/费晗/homework/slides-bigger/slides.js
+++ members/费晗/homework/slides-bigger/slides.js
@@ -1,29 +1,28 @@
-/**
- * 
- * @authors  ()
- * @date    2016-06-23
- * @version $Id$
- */
-$.fn.slides = function(){
-	var $menuItems = $('li.menu-items');
-	var $art = $('.art');
-	var width = $('.slides').width();
-	$menuItems.each(function(){
-		var $list = $(this)
-		$list.on('click',function(){
-			console.log($list)
-			// var current = $list.index();
-			// console.log(current) 
-			/*
-			 *这里不能用.index(),它返回的是当前元素的位置;
-			 *如果html一开始不是当前元素，分清需求是要返回
-			 *元素位置的值还是元素数组的下标
-			*/
-			var current = $list.attr('date-items');
-			var left = current*width;
-			$list.addClass('active').siblings('.active').removeClass('active');
-			$('.art').stop(true,true).animate({left: -left},300);
-		})
-	})
-	$('.menu-items:first').addClass('active').siblings().addClass('inact');
-}
+// /**
+//  * 
+//  * @authors  ()
+//  * @date    2016-06-23
+//  * @version $Id$
+//  */
+// $.fn.slides = function(){
+
+// 	var width = $('.slides').width();
+// 	$('li.menu-items').on('click',function(e){
+// 		console.log($(this))
+// 		var $current = $(e.currentTarget)
+// 		// var current = $list.index();
+// 		// console.log(current) 
+		
+// 		 *这里不能用.index(),它返回的是当前元素的位置;
+// 		 *如果html一开始不是当前元素，分清需求是要返回
+// 		 *元素位置的值还是元素数组的下标
+		
+// 		$(this).addClass('active').siblings('.active').removeClass('active');
+// 		var current = $current.attr('data-items');
+// 		var index =+current;
+// 		console.log('here')
+// 		console.log(index)
+// 		var left = index*920;
+// 		$('.art').stop(true,true).animate({marginLeft: -left},300)
+// 	})
+// }
