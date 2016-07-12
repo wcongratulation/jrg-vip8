0fefe66756e28e08dbdaa7ac6bda1205ab65883a
beyond <964959033@qq.com>
2016-07-12 21:04:11 +0800

    resume

--- members/费晗/homework/resume-tmp/js/animationHeader.js
+++ members/费晗/homework/resume-tmp/js/animationHeader.js
@@ -1,15 +1,17 @@
-
+/**==navShow==**/
 $.goShow = function(){
 	var $nav = $('header.siteTop-wrapper')
-	console.log($nav)
 	$(window).on('scroll',function(){
 		var offsetY = $(this).scrollTop();
-		console.log(offsetY)
-		if (offsetY>100) {
+		if (offsetY>200) {
 			$nav.addClass('visible');
 		}else{
 			$nav.removeClass('visible')
 		}
 	})
 }
+/**==gotoTop==**/
 $.goShow()
+$('.gotop-js').on('click',function(){
+	$('body').animate({scrollTop: 0},'slow')
+})
